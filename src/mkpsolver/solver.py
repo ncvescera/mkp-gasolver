1741d1bb406c52ee30da4bda54f361fbfe85b88f
Nicolò Vescera <nicolo.vescera@gmail.com>
2023-08-29 17:39:19 +0200

    Select New Population Function
    Implementata funzione per selezionare la nuova popolazione (select
    best).
    Completato sviluppo della funzione update best.

--- src/mkpsolver/solver.py
+++ src/mkpsolver/solver.py
@@ -9,23 +9,40 @@ class GeneticAlgorithm():
     def __init__(
             self,
             problem: MKProblem,  # TODO: si può omettere ?
+            num_elem=16,
             num_gen=100,
             pcross=.9,
             pmut=.01):
 
         self.problem = problem
         self.num_items = problem.get_dim()
+        self.num_elem = num_elem
         self.pcross = pcross
         self.pmut = pmut
         self.num_gen = num_gen
 
-    def init_population(self, num_elem: int = 16) -> List[Solution]:
-        self.population = []
-        self.f_obj = np.zeros(num_elem)
-        self.best = None
-        self.best_f = float('inf')  # huge number
+    def solve(self):
+        self.improvements = []
+        self.init_population()
 
-        for i in range(num_elem):
+        # print(self.population)
+
+        for gen in range(1, self.num_gen + 1):
+            mating_pool = self.select_mating_pool()
+            children = self.do_crossover(mating_pool)
+            self.do_mutation(children)
+            self.select_new_population(children, gen)
+
+        return None, None, None  # TODO: completare
+
+    def init_population(self):
+        self.population = []
+        # self.f_obj = np.zeros(self.num_elem)
+        self.f_obj = list(np.zeros(self.num_elem))
+        self.best = None
+        self.best_f = float('-inf')  # huge number
+
+        for i in range(self.num_elem):
             tmp_sol = np.zeros(self.num_items)
 
             # list of indexes (e.g. 1 means df[1])
@@ -55,21 +72,41 @@ class GeneticAlgorithm():
             self.population.append(tmp_sol)
             self.f_obj[i] = self.problem.objective_function(
                 tmp_sol)  # TODO: usare dict ?
+            self.update_best(tmp_sol, self.f_obj[i], 0)
 
-    def select_mating_pool(self):
+    def update_best(self, x: Solution, fx: float, gen: int):
+        # maximize the objective function
+        if fx > self.best_f:
+            self.best_f = fx
+            self.best = x
+            print(f"new best {fx} @ gent: {gen}")
+            self.improvements.append((
+                gen,
+                fx,
+            ))
+
+    def select_mating_pool(self) -> List[Tuple[Solution, Solution]]:
 
         def roulette_wheel():
             raise NotImplementedError
 
-        def tournament(k=5):
-            random_select_items = [
+        def tournament(k: int = 5) -> Solution:
+            random_select_solutions = [
                 random.randint(0,
                                len(self.population) - 1) for _ in range(k)
             ]
 
-            tmp = {i: self.f_obj[i] for i in random_select_items}
+            # generate a dictioray {solution index : solution fitness}
+            # e.g. solution 1/16 has fitness vale of 287
+            selected_objectivefunctions = {
+                i: self.f_obj[i]
+                for i in random_select_solutions
+            }
 
-            max_index = max(tmp, key=tmp.get)
+            # find solution index with max fitless value
+            # e.g. solution 3/16 has the max fintess value
+            max_index = max(selected_objectivefunctions,
+                            key=selected_objectivefunctions.get)
 
             return self.population[max_index]
 
@@ -116,7 +153,7 @@ class GeneticAlgorithm():
 
         return children
 
-    def do_mutation(self, children: List[Solution]) -> List[Solution]:
+    def do_mutation(self, children: List[Solution]):
         '''
         Randomly flip bits according to pmut probability
         '''
@@ -125,5 +162,27 @@ class GeneticAlgorithm():
                 if random.random() < self.pmut:
                     child[i] = int(not child[i])
 
+    # TODO: compleatre
     def repair_operator(self) -> Solution:
         raise NotImplementedError
+
+    def select_new_population(self, children: List[Solution], gen: int):
+
+        def select_best():
+            total_solutions: List[Solution] = self.population + children
+            total_fintesses: List[float] = self.f_obj + [
+                self.problem.objective_function(c) for c in children
+            ]
+
+            assert len(total_solutions) == len(total_fintesses)
+
+            total_indexes: List[int] = list(range(len(total_solutions)))
+            total_indexes.sort(key=lambda i: total_fintesses[i], reverse=True)
+            best_indexes: List[int] = total_indexes[:self.num_elem]
+
+            self.population = [total_solutions[i] for i in best_indexes]
+            self.f_obj = [total_fintesses[i] for i in best_indexes]
+
+            self.update_best(self.population[0], self.f_obj[0], gen)
+
+        select_best()
