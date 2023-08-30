e789dab85d951bdbb945e72b43208abb76a97fda
Nicolò Vescera <nicolo.vescera@gmail.com>
2023-08-30 15:57:33 +0200

    Aggiornato main.py
    Aggiornato main.py per fer eseguire il completamente il codice
    dell'algoritmo genetico

--- src/main.py
+++ src/main.py
@@ -11,9 +11,23 @@ def main(args):
     # TEST: objective function
     print(problem.objective_function(np.array([1, 1, 0, 0, 0, 0, 0, 0, 0, 0])))
 
-    solver = GeneticAlgorithm(problem)
-    solver.init_population()
-    print(solver.population)
+    solver = GeneticAlgorithm(problem, num_gen=100)
+    solution = solver.solve()
+    solution_items = problem.df[solution["best"] == 1]
+
+    print("\n### SOLUTION ###")
+    for key, value in solution.items():
+        print(f"{key}: {value}")
+
+    print("Selected Items:")
+    print(solution_items)
+    print(" ", solution_items.sum().to_numpy())
+
+    # solver.init_population()
+
+    # print(len(solver.population), solver.population)
+    # print(solver.f_obj)
+    # print(solver.select_mating_pool())
     # print(solver.do_crossover(np.array([1, 1, 0, 1]), np.array([0, 0, 1, 0])))
 
 
