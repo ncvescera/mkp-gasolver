4f45df5437db1e27f07aca8e67593cdfc51e671b
Nicolò Vescera <nicolo.vescera@gmail.com>
2023-09-08 12:25:37 +0200

    Tuning Data Analisys
    Iniziata scrittura della funzione che analizza i dati della fase di
    tuning dei parametri

--- src/tuning.py
+++ src/tuning.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import argparse
 from mkpsolver.problem_representation import MKProblem
 from mkpsolver.solver import GeneticAlgorithm
 from tqdm import tqdm
@@ -21,12 +22,12 @@ TUNING_SET = [
 DATA_FOLDER = "data"
 
 
-def main():
-    pmut = .01  # .03, .05
-    pcross = .9  # .97,
-    ngen = 250  # 250,
-    plen = 70  # 57,,,100
-    tk = 45  # 25, , 15, 61
+def tuning(pmut=.01, pcross=.9, ngen=250, plen=70, tk=45):
+    # pmut = .01  # .03, .05
+    # pcross = .9  # .97,
+    # ngen = 250  # 250,
+    # plen = 70  # 57,,,100
+    # tk = 45  # 25, , 15, 61
 
     result = []
 
@@ -68,5 +69,105 @@ def main():
     print(df)
 
 
+def show_result(path: str):
+    df = pd.read_csv(path, index_col=0)
+
+    print(df)
+    print()
+
+    success, fail = df['success'].value_counts()
+    all_success_ratio = success / (success + fail)
+    all_max_diff = max(df['diff'])
+
+    print(f"SUCCESS (#): {success}, "
+          f"FAIL (#): {fail}, "
+          f"SUCCESS RATIO (%): {all_success_ratio * 100}, "
+          f"MAX DIFF: {all_max_diff}")
+
+    df_groupby = df.groupby("file")
+    perfile_success = df.groupby(["file"])["success"].value_counts()
+    perfile_success = pd.DataFrame(perfile_success)
+    perfile_success["%"] = perfile_success['count'] / 5
+
+    print(perfile_success)
+    # return
+    perfile_minfound = df_groupby["found"].min()
+    perfile_maxfound = df_groupby["found"].max()
+
+    perfile_minmaxfound = pd.concat([perfile_minfound, perfile_maxfound],
+                                    axis=1)
+    perfile_minmaxfound.columns = ["min found", "max found"]
+
+    perfile_mindiff = df_groupby["diff"].min()
+    perfile_maxdiff = df_groupby["diff"].max()
+
+    perfile_minmaxdiff = pd.concat([perfile_mindiff, perfile_maxdiff], axis=1)
+    perfile_minmaxdiff.columns = ["min diff", "max diff"]
+
+    perfile_minmax = pd.concat([perfile_minmaxfound, perfile_minmaxdiff],
+                               axis=1)
+    print(perfile_minmax)
+    # print(df_groupby["found"].min(), df_groupby["found"].max())
+    # print(pd.concat([perfile_success, df_groupby["found"].min()], axis=1))
+
+
+def main(args):
+    match args.action:
+        case "tuning":
+            tuning(ngen=args.number_generation,
+                   plen=args.population_lenght,
+                   pcross=args.crossover_probability,
+                   pmut=args.mutation_probability,
+                   tk=args.tournament_k)
+        case "plot":
+            if args.input is None:
+                print("Missing Input file !")
+                return
+
+            show_result(args.input)
+        case _:
+            print("Wrong Action selected !")
+
+
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description='Parameter Tuning Script')
+
+    parser.add_argument(
+        'action',
+        type=str,
+        choices=["tuning", "plot"],
+        help=
+        'Action to run: Tuning Parameters (tuning) or Plotting Results (plot)')
+    parser.add_argument('-plen',
+                        '--population_lenght',
+                        default=16,
+                        type=int,
+                        help='Initial Population Lenght')
+    parser.add_argument('-pcross',
+                        '--crossover_probability',
+                        default=.9,
+                        type=float,
+                        help='Crossover Probability (from 0 to 1)')
+    parser.add_argument('-pmut',
+                        '--mutation_probability',
+                        default=.01,
+                        type=float,
+                        help='Mutation probability (from 0 to 1)')
+    parser.add_argument('-ngen',
+                        '--number_generation',
+                        default=100,
+                        type=int,
+                        help='Number of generations')
+    parser.add_argument('-tk',
+                        '--tournament_k',
+                        default=5,
+                        type=int,
+                        help='Tournament random solution to select')
+    parser.add_argument('-i',
+                        '--input',
+                        default=None,
+                        type=str,
+                        help='Path to result file. Needed if action=plot')
+
+    args = parser.parse_args()
+    main(args)
